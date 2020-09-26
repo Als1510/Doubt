cbd00c36932670b67c2607c7aef850542d1ec418
Anshul Sharma <61790929+Als1510@users.noreply.github.com>
2020-09-26 20:07:57 +0530

    Update _handelLogin.php

--- partials/_handelLogin.php
+++ partials/_handelLogin.php
@@ -18,17 +18,17 @@ if($_SERVER['REQUEST_METHOD']=="POST")
             $_SESSION['Sno'] = $row['Sno'];
             $_SESSION['useremail'] = $email;
             $_SESSION['user_name'] = $row['user_name'];
-            header("Location: /FORUM/index.php");
+            header("Location: /Doubt/index.php");
         }
         else
         {
             $showerror = "Wrong Password! Try again.";
-            header("Location: /index.php?login=false&error=$showerror");
+            header("Location: /Doubt/index.php?login=false&error=$showerror");
         }
     }
     else{
         $showerror = "Account not found. Signup";
-    header("Location: /index.php?login=false&error=$showerror");
+    header("Location: /Doubt/index.php?login=false&error=$showerror");
     }
 }
 ?>
