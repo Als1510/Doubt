65c174559296fc41589afffdf8bbaca9715b6594
Anshul Sharma <61790929+Als1510@users.noreply.github.com>
2020-09-26 20:01:31 +0530

    Update _handelSignup.php

--- partials/_handelSignup.php
+++ partials/_handelSignup.php
@@ -25,14 +25,14 @@ if($_SERVER['REQUEST_METHOD']=="POST")
             if($result)
             {
                 $showAlert = true;
-                header("Location: /FORUM/index.php?signupsuccess=true");
+                header("Location: /index.php?signupsuccess=true");
                 exit();
             }
         }
         else
         {
             $showError = "Passwords do not match";
-            header("Location: /FORUM/index.php?signupsuccess=false&error=$showError");
+            header("Location: /index.php?signupsuccess=false&error=$showError");
         }
     }
 }
