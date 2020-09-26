4499e0d1531104c4cdf35ede1e3ae1f1a9b1acc0
Anshul Sharma <61790929+Als1510@users.noreply.github.com>
2020-09-26 20:04:32 +0530

    Update _header.php

--- partials/_header.php
+++ partials/_header.php
@@ -11,10 +11,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-            <a class="nav-link" href="/FORUM">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/Doubt">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="/FORUM/about.php">About</a>
+            <a class="nav-link" href="/Doubt/about.php">About</a>
             </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -29,7 +29,7 @@
             echo '</div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="/FORUM/contact.php">Contact</a>
+            <a class="nav-link" href="/Doubt/contact.php">Contact</a>
         </li>
     </ul>
     <div class="row mx-2">';
