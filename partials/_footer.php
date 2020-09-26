9afa1976e4355c1aa49d15e15ef3454b297de889
Anshul Sharma <61790929+Als1510@users.noreply.github.com>
2020-09-26 20:00:22 +0530

    Update _footer.php

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,3 +1,3 @@
 <div class="container-fluid bg-dark text-light">
-    <p class="text-center py-2 mb-0">Copyright iDiscuss Coding Forum 2021 | All rights reserved.</p>
+    <p class="text-center py-2 mb-0">Copyright Doubt Discussion Forum 2021 | All rights reserved.</p>
 </div> 
